0653d3db6b03177e8c84c5b04fba1dcf0c03e14b
Rob Connolly <rob@webworxshop.com>
2019-09-16 21:04:43 +1200

    Write a couple of tests.

--- apps/test/test_motion_lights.py
+++ apps/test/test_motion_lights.py
@@ -1,6 +1,7 @@
 # import pytest
 from appdaemontestframework import automation_fixture
 from apps.motion_lights import MotionLight
+from datetime import time
 
 
 @automation_fixture(MotionLight)
@@ -10,5 +11,15 @@ def motion_light(given_that):
     given_that.passed_arg("timeout").is_set_to(120)
 
 
-def test_working(given_that, motion_light, assert_that):
-    assert True
+def test_callback_registered(given_that, motion_light, assert_that):
+    assert_that(motion_light).listens_to.state(
+        "binary_sensor.motion", new="on"
+    ).with_callback(motion_light.motion_callback)
+
+
+def test_light_on_off(given_that, motion_light, time_travel, assert_that):
+    motion_light.is_light_times = lambda: True
+    motion_light.motion_callback(None, None, None, None, None)
+    assert_that("light.test_light").was.turned_on()
+    time_travel.fast_forward(2).minutes()
+    assert_that("light.test_light").was.turned_off()
